52820d9c0a063db359b5c83cde5b8713d2e8c734
tkwiatek <lleqsnoom@gmail.com>
2020-06-26 10:58:25 +0200

    A W S

--- cpp/HxGlue.cpp
+++ cpp/HxGlue.cpp
@@ -27,6 +27,9 @@ HXredisReply *__command(redisContext *c, String cmd){
         std::cout << "\nNULL\n";
     }else{
         std::cout << "\n" << ((redisReply *)res)->type << "\n";
+        if((redisReply *)res)->type == 1){
+            std::cout << "\n" << ((redisReply *)res)->len << ":" << ((redisReply *)res)->str << "\n";
+        }
     }
     
 
